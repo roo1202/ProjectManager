083a21df9884d1b272fc6bf7917d9847839c039a
Manzano46 <kevinmanzano020610@gmail.com>
2024-09-06 14:58:36 -0400

    refactor: :art: carpeta faltante

--- Opportunity.py
+++
@@ -1,7 +0,0 @@
-
-from PMOntologic.Risk import Risk
-# Class representing an Opportunity in the project
-class Opportunity(Risk):
-    def __init__(self, probability=0.1, impact=[], events_or_conditions=[], benefits=[]):
-        super(self, probability, impact, events_or_conditions).__init__
-        self.benefits = benefits
---
+++ PMOntologic/Resource.py
@@ -0,0 +1,17 @@
+
+# Class representing a Resource in the project
+class Resource:
+    def __init__(self, name: str, total: int, cost: float):
+        self.name = name
+        self.total = total
+        self.cost = cost
+
+    def __str__(self):
+        return f"Resource(Cost: {self.cost})"
+    
+    def maximize_resource():
+        pass
+
+
+
+
